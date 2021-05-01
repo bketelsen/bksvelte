89409b87b5e75cedeba204611324ca18bb27f0e7
Brian Ketelsen <bketelsen@gmail.com>
2021-05-01 12:17:15 -0400

    categories

--- src/lib/components/TopNav/search.svelte
+++ src/lib/components/TopNav/search.svelte
@@ -33,6 +33,7 @@
 					{#each navigation as item}
 						<a
 							href={item.route}
+              sveltekit:prefetch
 							class="{item.route === $page.path
 								? 'border-indigo-500 text-gray-900'
 								: 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'} inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
@@ -139,6 +140,7 @@
 			{#each navigation as item}
 				<a
 					href={item.route}
+          sveltekit:prefetch
 					class="{item.route === $page.path?"bg-indigo-50 border-indigo-500 text-indigo-700":"border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800"}  block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
 					>{item.name}</a
 				>
