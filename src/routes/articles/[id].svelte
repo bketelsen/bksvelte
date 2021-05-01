7149bbafdbb1307bf7b2f4d72b7aed94f4a73c3b
Brian Ketelsen <bketelsen@gmail.com>
2021-05-01 09:40:27 -0400

    so much debugging

--- src/routes/articles/[id].svelte
+++ src/routes/articles/[id].svelte
@@ -1,6 +1,13 @@
 <script context="module">
   import { articles } from "$lib/dataStore";
-  export const prerender = true;
+  import {  dev } from '$app/env';
+
+// since there's no dynamic data here, we can prerender
+// it so that it gets served as a static asset in prod
+export const prerender = true;
+  // we don't need any JS on this page, though we'll load
+// it in dev so that we get hot module replacement...
+export const hydrate = dev;
   // see https://kit.svelte.dev/docs#loading
   export const load = async ({ fetch, page }) => {
     const article = await articles.getOne(page.params.id);
