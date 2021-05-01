d3bb06b4c54fb9f38131b0fde9e1b43648afaf7a
Brian Ketelsen <bketelsen@gmail.com>
2021-05-01 12:23:26 -0400

    no need to make network calls in layout

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,28 +1,4 @@
-<script context="module">
-/*	import {websites,categories}	 from '$lib/dataStore'
-
-	// see https://kit.svelte.dev/docs#loading
-	
-	export const load = async () => {
-
-		const website = await websites.getOne('brian.dev');
-			const cc = await categories.getAll();
-			return {
-				props:  {
-					website: website,
-					categories: cc
-				}
-			};
-
-	};
-*/
-</script>
-
 <script>
-
-	export let website;
-	export let categories;
-
 	const navigation = [
 		{name:"Home", route:"/"},
 		{name:"Blog", route:"/articles"},
@@ -30,8 +6,6 @@
 		{name:"About", route:"/about"},
 
 	]
-	
-
 	import Container from '$lib/components/Container/full.svelte'
 	import TopNav from '$lib/components/TopNav/search.svelte';
 	import Footer from '$lib/components/Footer/index.svelte';
