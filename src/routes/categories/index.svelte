8f77a553a8b7efa3ac149df6dd33856b506e34d4
Brian Ketelsen <bketelsen@gmail.com>
2021-05-03 15:24:32 -0400

    push

--- src/routes/categories/index.svelte
+++ src/routes/categories/index.svelte
@@ -20,22 +20,15 @@
 </script>
 
 <script>
-  export let categories;
+export let categories;
+import Simplecards from '$lib/components/Lists/Grid/simplecards.svelte';
 </script>
 
 <svelte:head>
   <title>Categories</title>
 </svelte:head>
 
-<div class="articles">
-  <h1>Categories</h1>
-  {#each categories as category (category.id)}
-    <div>
-      <h4><a href="/categories/{category.id}">{category.name}</a></h4>
-      <div>{category.description}</div>
-    </div>
-  {/each}
-</div>
+<Simplecards title="Categories" {categories}></Simplecards>
 
 <style>
 </style>
