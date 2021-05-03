968743024c2160f9d34a6ce3172c3129e9045eea
Brian Ketelsen <bketelsen@gmail.com>
2021-05-03 13:07:53 -0400

    cyan

--- src/lib/components/Sections/Content/centered.svelte
+++ src/lib/components/Sections/Content/centered.svelte
@@ -36,12 +36,12 @@
     <div class="relative px-4 sm:px-6 lg:px-8">
       <div class="text-lg max-w-prose mx-auto">
         <h1>
-          <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">{article.category.name}</span>
+          <span class="block text-base text-center text-cyan-600 font-semibold tracking-wide uppercase">{article.category.name}</span>
           <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">{article.title}</span>
         </h1>
         <p class="mt-8 text-xl text-gray-500 leading-8">{article.excerpt}</p>
       </div>
-      <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+      <div class="mt-6 prose prose-cyan prose-lg text-gray-500 mx-auto">
       <SvelteMarkdown source={article.body} />
       </div>
     </div>
