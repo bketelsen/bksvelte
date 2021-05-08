a31cc27dd80324b8f8e969c56f1046c88939e48f
Brian Ketelsen <bketelsen@gmail.com>
2021-05-08 07:57:15 -0400

    add alt about

---
+++ src/routes/altabout.svelte
@@ -0,0 +1,33 @@
+<script context="module">
+	export const prerender = true;
+	import {getPage} from '$lib/dataStore';
+
+	// see https://kit.svelte.dev/docs#loading
+	export const load = async () => {
+		const doc = getPage('about')
+            return {
+                props: {
+					page: doc,
+                },
+            };
+    }
+</script>
+
+<script>
+	export let page;
+	import SvelteMarkdown from 'svelte-markdown';
+	import { SplitPageWithImage } from 'components';
+</script>
+
+<svelte:head>
+	<title>{page.title}</title>
+</svelte:head>
+
+<SplitPageWithImage 
+	headline='This is the Headline' 
+	title='{page.title}' 
+	lede='{page.excerpt}' 
+	image='{page.image}'
+	>
+	<SvelteMarkdown source={page.body} />
+</SplitPageWithImage>
