a3bb16a1383d35ce587f8cecb95d4cac443a786e
Brian Ketelsen <bketelsen@gmail.com>
2021-05-10 12:06:53 -0400

    back to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,9 +12,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node({
-			out: 'api'
-		}),
+		adapter: adapter(),
 		prerender: {
 			force: true
 		}
