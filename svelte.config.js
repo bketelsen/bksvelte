f219b9a88a071644c3c8f8cb29c43965b3114577
Brian Ketelsen <bketelsen@gmail.com>
2021-05-10 11:57:46 -0400

    whatever

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,9 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node(),
+		adapter: node({
+			out: 'api'
+		}),
 		prerender: {
 			force: true
 		}
